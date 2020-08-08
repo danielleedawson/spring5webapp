7fe97e37cc0884059c644bb158bf6382c06f3c6a
danie <danie@LAPTOP-AHQUQ3RD>
2020-08-08 18:06:10 +0100

    Init Data

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring5webapp.domain;
 
 
+import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.Entity;
@@ -18,18 +19,17 @@ public class Author {
 	private String firstName;
 	private String lastName;
 	
-	@ManyToMany(mappedBy = "author")
-	private Set<Book> books;
+	@ManyToMany(mappedBy = "authors")
+	private Set<Book> books = new HashSet<Book>();
 	
 	public Author() {
 		super();
 	}
 
-	public Author(String firstName, String lastName, Set<Book> books) {
+	public Author(String firstName, String lastName) {
 		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
-		this.books = books;
 	}
 	
 	public Long getId() {
